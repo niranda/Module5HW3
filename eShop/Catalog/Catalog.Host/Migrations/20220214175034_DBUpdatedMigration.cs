451f20f5cdae1466474632ccc6ddb26243753386
niranda <teslatorgames@gmail.com>
2022-02-14 19:56:35 +0200

    updated migrations and fixed bugs

---
+++ eShop/Catalog/Catalog.Host/Migrations/20220214175034_DBUpdatedMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog.Host.Migrations
+{
+    public partial class DBUpdatedMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
