c81a46d4a4cd02bea6352e2ce1febaa2aea5d95a
niranda <teslatorgames@gmail.com>
2022-02-14 11:53:54 +0200

    added GetById, GetByBrand, GetByType, GetBrands, GetTypes methods

---
+++ eShop/Catalog/Catalog.Host/Data/IdItems.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.Host.Data
+{
+    public class IdItems<T>
+    {
+        public long TotalCount { get; init; }
+
+        public T? Data { get; init; }
+
+        public int? Id { get; init; }
+    }
+}
